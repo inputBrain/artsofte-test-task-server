a56f9578f9e3f502e38c75ecbc8c35d8664a3258
Alexander <vsany@meta.ua>
2024-08-07 04:56:40 +0300

    init

---
+++ Artsofte.Host/Program.cs
@@ -0,0 +1,38 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("*", corsPolicyBuilder =>
+    {
+        corsPolicyBuilder
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddResponseCompression();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseExceptionHandler("/error");
+app.UseHsts();
+
+app.UseResponseCompression();
+
+app.UseRouting();
+app.UseCors("*");
+app.MapControllers();
+
+app.Run();
